6202513069b8a3be48eb3afd110339387be63a6b
artassist <124621399+artassist@users.noreply.github.com>
2023-02-09 10:55:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,26 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+import openai
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Initialize the OpenAI API client
+openai.api_key = "<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>"
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# Create a text input field for the user to enter the text to be summarized
+text_input = st.text_area("Enter text to summarize:")
+# Use the GPT-3 API to summarize the text
+model_engine = "text-davinci-002"
+prompt = (f"summarize: {text_input}"
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+completions = openai.Completion.create(
+    engine=model_engine,
+    prompt=prompt,
+    max_tokens=1024,
+    n=1,
+    stop=None,
+    temperature=0.5,
+)
 
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Extract the summarized text from the API response
+message = completions.choices[0].text
+# Display the summarized text in the Streamlit app
+st.write("Summary:", message)          
